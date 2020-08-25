a42b3e6e5884c9e2b59ed6de96cdd7094d247a5b
chic-state-0895 <brookeabryski@gmail.com>
2020-08-25 19:22:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,8 +6,8 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0, 1, 2],
-  [3, 4, 5]
-  [6, 7, 8]
-  [3, 5, 7]
-  [0, 4, 8]
+  [3, 4, 5],
+  [6, 7, 8],
+  [3, 5, 7],
+  [0, 4, 8],
 ]
